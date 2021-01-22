76e3b49c87ee3ba0683552a5ca94e4d968b46af0
adihendro <adi21hendro@gmail.com>
2021-01-22 17:03:01 +0700

    feat: add capitalizeWithoutLib1

--- src/main/java/future/assignment/Driver.java
+++ src/main/java/future/assignment/Driver.java
@@ -1,7 +1,12 @@
 package future.assignment;
+import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Capitalization capitalization = new Capitalization();
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        capitalization.capitalizeWithoutLib1(text);
+
     }
 }
