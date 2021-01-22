543879899c9dda6120504bd83ebb358f02f761ff
heizzz <gautamabryan@gmail.com>
2021-01-22 17:04:27 +0800

    initial dependency

---
+++ src/main/java/future/assignment/Capitalization.java
@@ -0,0 +1,14 @@
+package future.assignment;
+
+public class Capitalization {
+
+    public Capitalization(){}
+
+    public void capitalizeWithLib(String sentence){
+
+    }
+
+    public void capitalizeWithoutLib(String sentence){
+
+    }
+}
