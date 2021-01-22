2ffdc82854de9f2a64433edd2828232c2d5c2979
heizzz <gautamabryan@gmail.com>
2021-01-22 17:29:41 +0800

    add method capitalizeWithoutLib2

--- src/main/java/future/assignment/Capitalization.java
+++ src/main/java/future/assignment/Capitalization.java
@@ -4,11 +4,27 @@ public class Capitalization {
 
     public Capitalization(){}
 
-    public void capitalizeWithLib(String sentence){
+    public void capitalizeWithLib1(String sentence){
 
     }
 
-    public void capitalizeWithoutLib(String sentence){
+    public void capitalizeWithLib2(String sentence){
+
+    }
+
+    public void capitalizeWithoutLib1(String sentence){
+
+    }
+
+    public void capitalizeWithoutLib2(String sentence){
+        String[] arr = sentence.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : arr) {
+            sb.append(Character.toUpperCase(s.charAt(0)))
+                    .append(s.substring(1)).append(" ");
+        }
+        System.out.println(sb);
 
     }
 }
--- src/main/java/future/assignment/Driver.java
+++ src/main/java/future/assignment/Driver.java
@@ -1,7 +1,12 @@
 package future.assignment;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Capitalization capitalization = new Capitalization();
+        Scanner sc= new Scanner(System.in);
+        String test= sc.nextLine();
+        capitalization.capitalizeWithoutLib2(test);
     }
 }
