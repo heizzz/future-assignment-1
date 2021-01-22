30f56c65fe75ed2691c022e371a66d63a94a664d
Ardiafrizky <ardiafrizky24@gmail.com>
2021-01-22 17:02:03 +0700

    add output2withLib

---
+++ src/main/java/future/assignment/Capitalization.java
@@ -0,0 +1,20 @@
+package future.assignment;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Capitalization {
+
+    public Capitalization(){}
+
+    public void capitalizeWithLib(String sentence){
+
+    }
+
+    public void capitalizeWithoutLib(String sentence){
+
+    }
+
+    public void capitalizeWithLib2(String sentence){
+        System.out.println(StringUtils.capitalize(sentence));
+    }
+}
