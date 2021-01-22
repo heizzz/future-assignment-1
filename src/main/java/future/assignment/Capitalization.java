2d5cc71c0fbd2391a91768777f5d318a72e6e1c1
Ardiafrizky <ardiafrizky24@gmail.com>
2021-01-22 17:26:32 +0700

    1withLib

--- src/main/java/future/assignment/Capitalization.java
+++ src/main/java/future/assignment/Capitalization.java
@@ -17,10 +17,28 @@ public class Capitalization {
         System.out.println(res);
     }
 
-    public void capitalizeWithoutLib(String sentence){
-
+    public void capitalizeWithLib2(String sentence){
+        System.out.println(WordUtils.capitalizeFully(sentence));
     }
 
-    public void capitalizeWithLib2(String sentence){
+    public void capitalizeWithoutLib1(String sentence){
+          String[] arr = sentence.split(" ");
+//        String[] arr2 = [];
+//        StringBuffer sb = new StringBuffer();
+//        System.out.println(arr[2]);
+        for (int i=0; i<arr.length; i++){
+            arr[i]=arr[i].toUpperCase();
+        }
+//        for (String s : arr) {
+            System.out.println(arr);
+//        }
+    }
+    public void capitalizeWithoutLib2(String sentence){
+
+        for (String s : arr) {
+            sb.append(Character.toUpperCase(s.charAt(0)))
+                    .append(s.substring(1)).append(" ");
+        }
+        System.out.println(sb);
     }
 }
